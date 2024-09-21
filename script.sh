7cf5f77879ce308fc63cf98bfeb2b908a8e3f8e4
MebratuAG <mebratuantoniyos@gmail.com>
2024-09-21 18:44:17 +0100

    Lets Commit
    Started gitting

---
+++ script.sh
@@ -0,0 +1,10 @@
+echo "Hello, I'm about to use script to create folder and files."
+mkdir evangadi-students
+cd evangadi-students
+touch test-1.txt test-2.txt
+echo "Hi, just wrote this using script from script.sh file" > test-1.txt
+mkdir project-1
+cd project-1
+touch index.html
+mkdir css
+echo "Thanks, I'm Done!"
